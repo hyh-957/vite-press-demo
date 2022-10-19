9432600fdf0c2eefc04d6effc714ad3ef1a07fdf
steve yh huang <steve_yh_huang@wistron.com>
2022-10-19 18:16:43 +0800

    feat: 导航栏

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,16 @@
+export default {
+  title: 'Hyh957',
+  description: 'Hyh957的blog',
+  themeConfig: {
+    logo: '/tea.svg',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Archives', link: '/pages/archives' },
+      { text: 'Tags', link: '/pages/tags' },
+      { text: 'About', link: '/pages/about' }
+    ],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+}
